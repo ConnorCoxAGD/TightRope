5a92811a9507af510b4038d09300d06f818da147
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2023-02-02 20:18:39 -0800

    Crouch height adjustable -- kind of

--- Assets/PlayerControllerExtras.cs
+++ Assets/PlayerControllerExtras.cs
@@ -2,9 +2,11 @@ using Hertzole.GoldPlayer;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
 using UnityEngine.Windows;
 
 namespace Cox.ControllerProject.GoldPlayerAddons {
@@ -15,6 +17,7 @@ namespace Cox.ControllerProject.GoldPlayerAddons {
         [Header("Obstacle Detection")]
         [Tooltip("Distance from the player that an 'obstacle' must be to be interacted with. Used for crouching and mantling.")]
         [SerializeField] float obstacleDetectionDistance = 1.5f;
+        float modifiedCrouchHeight, presetCrouchHeight;
         GoldPlayerController goldPlayerController; //#Critical: required for player movement.
         CameraMovement cameraMovement;
 
@@ -26,25 +29,55 @@ namespace Cox.ControllerProject.GoldPlayerAddons {
                 Debug.LogError($"No GoldPlayerController component found for {gameObject.name}{this}.\nThis is required for player control.");
                 return;
             }
+            presetCrouchHeight = goldPlayerController.Movement.CrouchHeight;
+            modifiedCrouchHeight = goldPlayerController.Movement.CrouchHeight;
             cameraMovement = GetComponentInChildren<CameraMovement>();
             if (cameraMovement == null) {
                 Debug.LogWarning($"No CameraMovement component found for {gameObject.name}{this}.");
             }
+            goldPlayerController.Movement.OnBeginCrouch += Crouch;
+        }
+        private void OnDestroy() {
+            goldPlayerController.Movement.OnBeginCrouch -= Crouch;
         }
 
         // Update is called once per frame
-        void Update() {
-            DetectObstacles();
+        void LateUpdate() {
+            goldPlayerController.Controller.center = new Vector3(0, goldPlayerController.Controller.height / 2, 0);
+        }
+
+        public void Crouch() {
+            if (DetectObstacles()) {
+                goldPlayerController.Movement.CrouchHeight = FindCrouchHeight();
+                Debug.Log($"CrouchHeight set to {goldPlayerController.Movement.CrouchHeight}");
+            }
+
+
         }
 
         bool DetectObstacles() {
             RaycastHit hit;
-            if (Physics.SphereCast(new Vector3(transform.position.x, transform.position.y, transform.position.z), 2, transform.forward, out hit, obstacleDetectionDistance)) {
-                Debug.Log($"Obstacle detected: {hit.collider.gameObject.name}");
+            if (Physics.SphereCast(new Vector3(transform.position.x, transform.position.y + goldPlayerController.Controller.height, transform.position.z), 1, transform.forward, out hit, obstacleDetectionDistance)) {
+                //Debug.Log($"Obstacle detected: {hit.collider.gameObject.name}");
                 return true;
 
             }
             return false;
+        }
+        float FindCrouchHeight() {
+            if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + goldPlayerController.Controller.height / 2, transform.position.z), transform.forward + Vector3.down / 2, out RaycastHit groundHit)) {
+                Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + goldPlayerController.Controller.height / 2, transform.position.z), transform.forward + Vector3.down / 2, Color.red);
+                if (Physics.Raycast(groundHit.point, Vector3.up, out RaycastHit ceilingHit, presetCrouchHeight)) {
+                    Debug.DrawRay(groundHit.point, Vector3.up, Color.green);
+                    Debug.Log($"Height should be set to {Vector3.Distance(groundHit.point, ceilingHit.point)}.");
+                    var height = Vector3.Distance(groundHit.point, ceilingHit.point) - 0.35f;
+                    if (height > 0.81f) return height;
+                    return 0.81f;
+                }
+                Debug.Log($"No Ceiling: Height should be set to {presetCrouchHeight}.");
+                return presetCrouchHeight;
+            }
+            return presetCrouchHeight;
         }
     }
 }
